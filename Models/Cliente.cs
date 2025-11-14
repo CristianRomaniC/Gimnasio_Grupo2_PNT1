76446d17c4e38156ff06b9aa4de9cf63ac318d5f
Cristian Romani Cespedes <42820951@comunidadort.edu.ar>
2025-11-13 22:17:19 -0300

    Cambio Cristian 13-11

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic; // ¡Importante para las Listas!
 namespace GimnasioGrupo2.Models
 {
     // Hereda de Persona (Dni, Nombre, etc.)
+    //Hice Cambio Cristian
     public class Cliente : Persona
     {
         // --- Propiedades Propias de Cliente ---
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 // --- PASO 1 (NUEVO): Agregar los 'using' ---
 using Microsoft.EntityFrameworkCore;
 using GimnasioGrupo2.Data;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,9 +18,6 @@ builder.Services.AddDbContext<GimnasioContext>(options =>
 );
 // --- FIN DE LOS PASOS NUEVOS ---
 
-// Elimina la segunda declaración de 'builder' para evitar el error CS0128
-// var builder = WebApplication.CreateBuilder(args); <-- Esta línea se elimina
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -33,6 +32,21 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Serve static files from wwwroot
+app.UseStaticFiles();
+
+// Also expose a project-level "images" folder (outside wwwroot) at the URL path "/images"
+var imagesPath = Path.Combine(builder.Environment.ContentRootPath, "images");
+if (Directory.Exists(imagesPath))
+{
+    app.UseStaticFiles(new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(imagesPath),
+        RequestPath = "/images"
+    });
+}
+
 app.UseRouting();
 
 app.UseAuthorization();
